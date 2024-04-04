84fef12abc9291f50f6a949291a527da7ab45abc
akimajid <akimajid07@gmail.com>
2024-04-04 10:37:21 +0700

    Add split array into odd and even function

--- Soal1.js
+++ Soal1.js
@@ -1,7 +1,28 @@
-let arr = [];
-while(arr.length < 100){
-    let r = Math.floor(Math.random() * 100) + 1;
-    if(arr.indexOf(r) === -1) arr.push(r);
+function randomNumbers() {
+  let arrNumbers = [];
+  
+  for (let i = 0; i < 100; i++) {
+    arrNumbers.push(Math.floor(Math.random() * 50 + 1))
+  }
+  return arrNumbers
 }
-console.log(arr);
 
+function splitOddAndEven(num) {
+  let arrOdd = [];
+  let arrEven = [];
+
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 0) {
+      arrEven.push(num[i]);
+    } else {
+      arrOdd.push(num[i]);
+    }
+  }
+  const returnObject = {
+    arrOdd,
+    arrEven,
+  };
+  return returnObject;
+}
+
+console.log(splitOddAndEven(randomNumbers()));
